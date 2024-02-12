e6e88393a3b076bd31d56b3502220a61fe01fd51
TitanSlayer10 <130647793+TitanSlayer10@users.noreply.github.com>
2024-02-12 11:51:08 +0100

    Add header

---
+++ offers.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>rentAserv</title>
+    <link href="style/style.css" rel="stylesheet" />
+</head>
+<body>
+    <header class="header">
+        <div class="left-section">
+            <h2><a href="index.php">Accueil</a></h2>
+            <h2 id="onclick"><a href="offers.php">Offres</a></h2>
+            <h2><a href="myserv.php">Mes Serveurs</a></h2>
+        </div>
+        <div class="right-section">
+            <h2><a href="user.php">Utilisateur</a></h2>
+        </div>
+    </header>
+</body>
+</html>
