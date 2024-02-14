3dcd3bc2ff6223981e263ed07e106cd9e63f0b19
LikAnda <98820554+LikAnda@users.noreply.github.com>
2024-02-14 11:42:53 +0100

    Register, Login and Logout

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>rentAserv</title>
+    <link href="style/loginform.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Déconnection réussie !</h2>
+    </div>
+</body>
+</html>
--- user.php
+++ user.php
@@ -2,31 +2,29 @@
 include "database.php";
 session_start();
 
-if(isset($_SESSION['user_id'])) {
-    $user_connected = true;
-    exit;
-}
+if (isset($_POST['username']) && isset($_POST['password'])) { // vérifier si les données du formulaire existe
+    echo "ISSET USERNAME AND PASSWORD";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') { // vérifier si le formulaire avec la method post a été soumis
-    if ((isset($_POST['username']) && (isset($_POST['password'])))) { // vérifier si les données du formulaire existe
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        //vérifier le compte el se connecter
-        $requete = $bdd->query("SELECT * FROM `utilisateurs` WHERE `Username` = '".$username."' AND `Password` = '".$password."'");
-        $user_nb = $requete->fetch()[0];
-        if ($user_nb >= 1) {
-            $user_info = $requete->fetch();
-            $user_id = $user_info['id'];
-            $_SESSION[('user_id')] = $user_id;
-            echo "CONECTER";
-        }
-        header("Location: {$_SERVER['REQUEST_URI']}", true, 303); // utilisation de header() pour rediriger sur la page actuelle avec le code de status HTTP 303
-        exit(); // termine le script
+    //vérifier le compte el se connecter
+    $requete = $bdd->prepare("SELECT * FROM `utilisateurs` WHERE `Username` = ? AND `Password` = ?");
+    $requete->execute([$username, $password]);
+    $user_info = $requete->fetch();
+    if ($user_info) { // vérifier si la valeur n'est pas nulle
+        $_SESSION['user_id'] = $user_info['id'];
+        $_SESSION['username'] = $user_info['Username'];
+        $_SESSION['password'] = $user_info['Password'];
+        $_SESSION['last_name'] = $user_info['Nom'];
+        $_SESSION['first_name'] = $user_info['Prenom'];
+        echo "CONNECTER";
     }
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303); // utilisation de header() pour rediriger sur la page actuelle avec le code de status HTTP 303
+    exit(); // termine le script
 }
 
-if ((isset($_POST['re-username']) && (isset($_POST['re-password'])) && (isset($_POST['re-last-name']) && (isset($_POST['re-first-name']))))) { // vérifier si les données du formulaire existe
+if (isset($_POST['re-username']) && isset($_POST['re-password']) && isset($_POST['re-last-name']) && isset($_POST['re-first-name'])) { // vérifier si les données du formulaire existe
+    echo "<h1>ISSET REGISTER</h1>";
     $id = create_id($bdd, 'utilisateurs');
     $password = $_POST['re-password'];
     $username = $_POST['re-username'];
@@ -35,6 +33,12 @@ if ((isset($_POST['re-username']) && (isset($_POST['re-password'])) && (isset($_
 
     $requete = "INSERT INTO `utilisateurs` (`id`, `Username`, `Password`, `Nom`, `Prenom`) VALUES ('".$id."', '".$username."', '".$password."', '".$last_name."', '".$first_name."')";
     $bdd->exec($requete);
+
+    $_SESSION['user_id'] = $id;
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    $_SESSION['last_name'] = $last_name;
+    $_SESSION['first_name'] = $first_name;
 
     header("Location: {$_SERVER['REQUEST_URI']}", true, 303); // utilisation de header() pour rediriger sur la page actuelle avec le code de status HTTP 303
     exit(); // termine le script
@@ -62,14 +66,25 @@ if ((isset($_POST['re-username']) && (isset($_POST['re-password'])) && (isset($_
 
     <?php
     if(isset($_SESSION['user_id'])) {
-        echo "<h2>Vous êtes connecté ! (User id: ".$_SESSION['user_id'].")</h2>";
+        echo "<h2>Vous êtes connecté(e) ! (User id: ".$_SESSION['user_id'].")</h2>";
+        echo "<h2>Nom d'utilisateur : ".$_SESSION['username']."</h2>";
+        echo "<h2>Nom : ".$_SESSION['last_name']."</h2>";
+        echo "<h2>Prénom : ".$_SESSION['first_name']."</h2>";
+
+        echo <<< HTML
+        <div class="contain-center">
+            <form action="logout.php" method="POST">
+                <input class="button" type="Submit" value="Se déconnecter">
+            </form>
+        </div>
+        HTML;
     } else {
         echo <<< HTML
         <h2>Pas encore de compte ?</h2>
         <div class="contain-center">
             <a class="button center-button" href="login.php">Login</a>
             <a class="button center-button" href="register.php">Register</a>
-        </div>"
+        </div>
         HTML;
     }
     ?>
