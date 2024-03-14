238ea5523d7f0a1403fbe9c0f5d1d48acf5b2d40
LikAnda <98820554+LikAnda@users.noreply.github.com>
2024-03-14 15:13:43 +0100

    Add bdd schem img + repo link

--- index.php
+++ index.php
@@ -31,6 +31,11 @@ session_start();
         <p class="text"><b>rentAserv</b> est une plateforme de location de serveurs dédiés. Que vous ayez besoin d'un serveur pour héberger un site web, exécuter des applications ou effectuer des calculs intensifs, nous avons ce qu'il vous faut.<br>Nos serveurs sont puissants, fiables et flexibles, et notre équipe est là pour vous aider à trouver la solution la mieux adaptée à vos besoins.</p>
         <p class="text">Parcourez nos offres et trouvez le serveur parfait pour votre projet. Louez dès aujourd'hui et bénéficiez d'une infrastructure de qualité professionnelle à un prix abordable.</p>
     </div>
+    <br>
+    <div class="center">
+        <h3>Repo GitHub : <a href="https://github.com/LikAnda/rentAserv" target="_blank">https://github.com/LikAnda/rentAserv</a></h3>
+        <img class="center" src="db-schem.png" alt="Image" height="600" width="800">
+    </div>
 
 </body>
 </html>
